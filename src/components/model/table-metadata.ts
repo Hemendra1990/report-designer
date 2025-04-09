2c3b75260c9a45db080aa976fb71ccb407be3eca
subhamashu <subham.samantaray@bipros.com>
2025-04-09 20:26:34 +0530

    table metadata class.

---
+++ src/components/model/table-metadata.ts
@@ -0,0 +1,46 @@
+
+  export interface ColumnMetaData {
+    id: string;
+    columnName: string;
+    headerName: string;
+    dataType: string;
+    returnType: string;
+    formulaExpression: string;
+    constraints: string;
+    defaultValue: string;
+    staticOptions: string;
+    referenceAPI: string;
+    controlType: string;
+    selectedConstraints: string[];
+    staticOption: boolean;
+    standardColumn: boolean;
+    fieldWidth: string;
+    isFormula: boolean;
+    fractionalLength: number;
+    supportedType: string;
+    fileSize: number | null;
+    isFormulaValid: boolean;
+    dropdownOptionsOrderBy?: string;
+    helpText: string;
+    valueLength?: number;
+  }
+  
+  export interface RelationshipMetaData {
+    id: string;
+    type: string;
+    referenceTable: string;
+    sourceColumn: string;
+    referenceColumn: string;
+  }
+  
+  export interface TableMetaData {
+    id: string;
+    tableName: string;
+    displayName: string;
+    namePattern: string;
+    icon: string;
+    listDisplayName: string;
+    standardTable: boolean;
+    columns: ColumnMetaData[];
+    relationships: RelationshipMetaData[];
+  }
