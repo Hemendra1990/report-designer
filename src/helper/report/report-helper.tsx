ac3004dcb9c32488e141cafafb67e3a7a17c7277
Aswini Biswal <aswini.biswal@bipros.com>
2025-04-15 11:19:05 +0530

    filter save update impl

--- src/helper/report/report-helper.tsx
+++ src/helper/report/report-helper.tsx
@@ -1,8 +1,10 @@
-import { Report } from "@/components/model/report"
+import { Field } from "@/app/(secure)/report-builder/model/Field";
+import { Filter } from "@/app/(secure)/report-builder/model/Filter";
+import { Report, ReportFilter } from "@/components/model/report"
 import { ReportType } from "@/components/model/report-type"
 import { report } from "process";
 
-export const generateReportPayload = (reportId: string, reportName: string, reportType: ReportType, sqlQuery: string, columns, groups, filters): Report => {
+export const generateReportPayload = (reportId: string, reportName: string, reportType: ReportType, sqlQuery: string, columns, groups, filters: Filter[]): Report => {
     let updatedColumns = columns.map(column => {
         delete column.id;
         return column;
@@ -13,9 +15,14 @@ export const generateReportPayload = (reportId: string, reportName: string, repo
         return group;
     })
 
-    let updatedFilters = filters.map(filter => {
-        delete filter.id;
-        return filter;
+    let updatedFilters: ReportFilter[] = filters.map(filter => {
+        let reportFilter: ReportFilter = {
+            tableName: filter.field.tableName,
+            columnName: filter.field.columnName,
+            operator: filter.operator,
+            value: filter.value,
+        }
+        return reportFilter;
     })
 
     let reportPayload: Report = {
@@ -32,4 +39,16 @@ export const generateReportPayload = (reportId: string, reportName: string, repo
         filters: updatedFilters
     }
     return reportPayload;
+}
+
+export const generateFilterJson = (reportFilters: ReportFilter[], reportFields: Field[]): Filter[] => {
+    return reportFilters.map(reportFilter => {
+        let filter: Filter = {
+            id: reportFilter.id,
+            field: reportFields.find(field => field.tableName === reportFilter.tableName && field.columnName === reportFilter.columnName),
+            operator: reportFilter.operator,
+            value: reportFilter.value
+        }
+        return filter;
+    })
 }
